d15b9428a501ed3975a89df9708e0d94918fc48e
Fatih <fatih_bas37@outlook.de>
2020-11-22 21:46:51 +0100

    begin Aufgabe_3

---
+++ Aufgabe_3/Fixkosten App/Fixkosten App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Fixkosten App
+//
+//  Created by Fatih Bas on 22.11.20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
